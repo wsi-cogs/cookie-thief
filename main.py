2bc1b5d2e6b333f7f69ab2731671f12ffa5a1943
sb48 <sb48@sanger.ac.uk>
2018-12-13 08:35:07 +0000

    Add munch endpoint

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ async def steal(request):
         raise HTTPBadRequest(text="Requires cookie param")
     return web.Response(text=request.cookies.get(name, ""))
 
+
+async def munch(request):
+    name = request.query.get("cookie", None)
+    if name is None:
+        raise HTTPBadRequest(text="Requires cookie param")
+    resp = web.HTTPFound(location="/login")
+    resp.del_cookie(name=name)
+    return resp
+
+
 app = web.Application()
-app.add_routes([web.get('/', steal)])
+app.add_routes([web.get('/steal', steal)])
+app.add_routes([web.get('/munch', munch)])
 web.run_app(app, host="0.0.0.0", port=8000)
